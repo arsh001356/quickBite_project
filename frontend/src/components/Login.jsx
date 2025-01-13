59d7112059e7497f9189efc16e126fdb4bbd4472
arsh001356 <arsh001356@gmail.com>
2025-01-13 19:42:26 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
             password: passwordRef.current.value,
         };
 
-        await axios.post("http://localhost:5000/user/login", formData)
+        await axios.post("https://quickbite-4ynl.onrender.com/user/login", formData)
             .then((res) => {
                 console.log(res.data)
                 if (res.data) {
